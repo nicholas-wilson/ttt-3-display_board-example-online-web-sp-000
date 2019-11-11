3020e77ee6f2cd63f7c691bd7a1b37f58626cbf4
wondrous-operand-3019 <nicholas.wilson@nevada.unr.edu>
2019-11-11 19:34:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  2.times do
-    print("   |   |   \n-----------\n")
-  end
-  print("   |   |   "\n)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
 display_board
